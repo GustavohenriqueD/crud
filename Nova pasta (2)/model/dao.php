642b60c88ab4dc263bf6c65d26bd9acf0bf53869
Gustavo <gustavin_dantass@hotmail.com>
2017-12-17 11:11:44 -0200

    crud

---
+++ Nova pasta (2)/model/dao.php
@@ -0,0 +1,69 @@
+<?php
+	class Dao {
+		
+		//Declaração dos atributos
+		private $servidor 	= "";
+		private $usuario 	= "";
+		private $senha 		= "";
+		private $nome_banco = "";
+		private $conexao;
+		private $resultado;
+		private $sql;
+		
+		//Método Construtor
+		private function __construct() {}
+		
+		//Método que abre conexão com banco
+		public function openConn() {
+			//Realizo a conexão e a seleção do banco de dados
+			$this->conexao = new mysqli($this->servidor, 
+				$this->usuario, $this->senha, $this->nome_banco);
+				
+			//Verifico se não houve erros
+			if ($this->conexao->connect_errno) {
+				printf("Falha na conex&atilde;o:\n%s", 
+					$this->conexao->connect_error);
+				die;
+			}
+		}
+		
+		//Método para fechar conexão
+		public function closeConn() {
+			return $this->conexao->close();
+		}
+		
+		//Método para execução (INSERT, UPDATE and DELETE)
+		public function execute($sql) {
+			$this->openConn(); //abre conexao
+			$retorno = $this->conexao->query($sql); //executa o comando
+			$this->closeConn(); //fechar a conexão
+			
+			return $retorno;
+		}
+		
+		//Método para consulta (SELECT)
+		public function executeS($sql) {
+			$this->openConn(); //abre conexao
+			$retorno = $this->conexao->query($sql); //executa o comando
+			
+			if ($retorno) { //Caso retorne a coleção de dados
+				$array_dados = array();
+				while($objeto = $retorno->fetch_object()) {
+					$array_dados[] = $objeto;					
+				}
+				$this->closeConn();
+				return $array_dados;
+			
+			} else { //Caso não tenha retorno de coleção de dados
+				$this->closeConn();
+				return false;
+			}
+		}
+		
+		//Método padrão SINGLETON (garante apenas 1 instancia de objeto)
+		public static function getInstancia() {
+			return new Dao();
+		}
+	}
+	
+?>
